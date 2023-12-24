ff9fa8d2db0cbe6b485303c802418acd8defaf25
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-12-24 17:41:59 +0900

    Design : 프로필 설명 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
 import profileImage from "../../public/images/profile.png";
+import Link from "next/link";
 
 export default function AA() {
   return (
-    <div className="">
-      <Image
-        src={profileImage}
-        alt="profile"
-        className="mx-auto my-1 rounded-full w-60"
-      ></Image>
+    <div className="mx-auto my-1 w-60 text-center">
+      <Image src={profileImage} alt="profile" className="rounded-full" />
+      <h1 className="text-2xl font-bold mt-1">제주당근오렌</h1>
+      <h1 className="font-bold">Web FrontEnd Enginner</h1>
+      <p className="mb-1">안녕하세요! 잘 부탁드립니다!</p>
+      <Link
+        href="/contact"
+        className="bg-amber-400 px-3 py-1 font-bold rounded-lg"
+      >
+        Contact Me!
+      </Link>
     </div>
   );
 }
