2e783ee6d18c2e93ad0ab82c494f6b9a8bc4bf80
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-12-24 17:03:07 +0900

    Design : 헤더 및 프로필 이미지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
+import profileImage from "../../public/images/profile.png";
 
 export default function AA() {
-  return <div></div>;
+  return (
+    <div className="">
+      <Image
+        src={profileImage}
+        alt="profile"
+        className="mx-auto my-1 rounded-full w-60"
+      ></Image>
+    </div>
+  );
 }
