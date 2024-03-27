2ab22ef3b8cbc68c57752687d9c048b2f6b68ebd
CMG_NB <c9010912@gmail.com>
2024-03-27 21:55:12 +0900

    Feat : 포스트 상세 데이터 패칭

--- src/app/posts/[postPath]/page.tsx
+++ src/app/posts/[postPath]/page.tsx
@@ -1,9 +1,38 @@
+import { PostType } from "@/app/page";
+
 type Props = {
   params: {
     postPath: string;
   };
 };
 
-export default function PostDetailPage({ params }: Props) {
-  return <div>{params.postPath}</div>;
+export default async function PostDetailPage({ params }: Props) {
+  const postDetailDataFetch = async () => {
+    return await fetch(`http://localhost:3000/posts/posts.json"`)
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const postDescriptionDataFetch = async () => {
+    return await fetch(`http://localhost:3000/posts/${params.postPath}.md`)
+      .then((response) => response.text())
+      .then((data) => {
+        return data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const mockPostData: PostType[] = await postDetailDataFetch();
+  const postDetailData: PostType | undefined = mockPostData.find((postData) => {
+    return (params.postPath = postData.path);
+  });
+
+  return <div>asdf</div>;
 }
