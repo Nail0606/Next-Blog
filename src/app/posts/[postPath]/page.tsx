0351469303a6b3922d798d6ec9c45cb99f36b008
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-03-29 20:37:31 +0900

    Feat: 상세 글 markdown 적용

--- src/app/posts/[postPath]/page.tsx
+++ src/app/posts/[postPath]/page.tsx
@@ -1,4 +1,8 @@
 import { PostType } from "@/app/page";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 type Props = {
   params: {
@@ -8,7 +12,7 @@ type Props = {
 
 export default async function PostDetailPage({ params }: Props) {
   const postDetailDataFetch = async () => {
-    return await fetch(`http://localhost:3000/posts/posts.json"`)
+    return await fetch("http://localhost:3000/posts/posts.json")
       .then((response) => response.json())
       .then((data) => {
         return data;
@@ -29,10 +33,58 @@ export default async function PostDetailPage({ params }: Props) {
       });
   };
 
-  const mockPostData: PostType[] = await postDetailDataFetch();
-  const postDetailData: PostType | undefined = mockPostData.find((postData) => {
-    return (params.postPath = postData.path);
-  });
+  const mockPostData: PostType[] | undefined = await postDetailDataFetch();
+  const postDetailData: PostType | undefined = mockPostData?.find(
+    (postData) => {
+      return params.postPath === postData.path;
+    }
+  );
+  const description = await postDescriptionDataFetch();
+  console.log(params.postPath);
 
-  return <div>asdf</div>;
+  return (
+    description && (
+      <div>
+        <ReactMarkdown
+          remarkPlugins={[remarkGfm]}
+          components={{
+            code({ node, inline, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(
+                className !== undefined ? className : ""
+              );
+              if (!inline && match) {
+                return (
+                  <SyntaxHighlighter
+                    {...props}
+                    language={match[1]}
+                    PreTag="div"
+                  >
+                    {String(children).replace(/\n$/, "")}
+                  </SyntaxHighlighter>
+                );
+              } else if (inline === true) {
+                return (
+                  <SyntaxHighlighter
+                    {...props}
+                    language="typescript"
+                    PreTag="span"
+                  >
+                    {String(children)}
+                  </SyntaxHighlighter>
+                );
+              } else {
+                return (
+                  <code {...props} className={className}>
+                    {description}
+                  </code>
+                );
+              }
+            },
+          }}
+        >
+          {description}
+        </ReactMarkdown>
+      </div>
+    )
+  );
 }
